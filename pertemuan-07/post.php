c220bcdda90ddd9373461ebaf459438c9fcd1fd0
Reynanda Oktarian <2511500054@mahasiswa.atmaluhur.ac.id>
2025-11-11 20:04:23 +0700

    isset

--- pertemuan-07/post.php
+++ pertemuan-07/post.php
@@ -1,10 +1,19 @@
 <?php
   session_start();
-  $sesname = $_SESSION["nama"];
-  $sesemail = $_SESSION["email"];
-  $sespesan = $_SESSION["pesan"];
-?>
+  $sesname = "";
+  if (isset($_SESSION["nama"])):
+      $sesname = $_SESSION["nama"];  
+  endif;
+  
+   $sesemail = "";
+  if (isset($_SESSION["email"])):
+    $sesemail = $_SESSION["email"];
+  endif;
 
+  $sespesan = "";
+  if (isset($_SESSION["pesan"])):
+    $sespesan = $_SESSION["pesan"];
+  endif;
 <!DOCTYPE html>
 <html lang="en">
 
