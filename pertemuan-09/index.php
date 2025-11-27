fef22c8efff6e71780efebc7bf0b5281bcfd0f74
Reynanda Oktarian <2511500054@mahasiswa.atmaluhur.ac.id>
2025-11-27 12:18:43 +0700

    Menambahkan __DIR__ di file Index

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -124,7 +124,7 @@ $fieldConfig = [
 
   <section id="about">
       <h2>Tentang Saya</h2>
-      <?= tampilkanBiodata(conf: $fieldConfig, arr: $biodata) ?>
+      <?= tampilkanBiodata($fieldConfig, $biodata) ?>
     </section>
 
     <section id="contact">
