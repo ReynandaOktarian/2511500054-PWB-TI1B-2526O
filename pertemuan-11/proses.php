4ce3b93bf814893074db0ef34ce2964a71a3144f
Reynanda Oktarian <2511500054@mahasiswa.atmaluhur.ac.id>
2025-12-11 12:18:39 +0700

    membuat validasi (tidak boleh kosong, email valid)

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -13,3 +13,30 @@ $nama = bersihkan($_POST['txtNama'] ?? '');
 $email = bersihkan($_POST['txtEmail'] ?? '');
 $pesan = bersihkan($_POST['txtPesan'] ?? '');
 
+$errors = [];
+
+if ($nama === '') {
+  $errors[] = 'Nama wajib diisi.';
+}
+
+if ($email === '') {
+  $errors[] = 'Email wajib diisi.';
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $errors[] = 'Format e-mail tidak valid.';
+}
+
+if ($pesan === '') {
+  $errors[] = 'Pesan wajib diisi.';
+}
+
+if (!empty($errors)) {
+  $_SESSION['flash_error'] = implode('<br>', $errors);
+  $_SESSION['old'] = [
+    'nama' => $nama,
+    'email' => $email,
+    'pesan' => $pesan,
+  ];
+
+  $_SESSION['flash_error'] = implode('<br>', $errors);
+  redirect_ke('index.php#contact');
+}
